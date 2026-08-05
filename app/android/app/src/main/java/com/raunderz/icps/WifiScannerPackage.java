ecf35ce139c913f877b22b201a03c60aaf434f40
ishandas994-cloud <ishandas994@gmail.com>
2026-08-05 14:22:34 +0530

    wifiscannerpackage

--- app/android/app/src/main/java/com/raunderz/icps/WifiScannerPackage.java
+++ app/android/app/src/main/java/com/raunderz/icps/WifiScannerPackage.java
@@ -1,3 +1,17 @@
-public class WifiScannerPackage {
-    
+package com.raunderz.icps;
+
+import com.facebook.react.BaseReactPackage;
+import com.facebook.react.bridge.ModuleSpec;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class WifiScannerPackage extends BaseReactPackage {
+  @Override
+  public List<ModuleSpec> getNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<ModuleSpec>asList(
+      new ModuleSpec(WifiScannerModule.class, () -> new WifiScannerModule(reactContext))
+    );
+  }
 }
